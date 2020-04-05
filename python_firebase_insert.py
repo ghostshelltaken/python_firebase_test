e4fb6b7c000b0762cb05d4962677ab9debd15db9
Chaitanya Dodiya <chaitanya_dodiya>
2020-04-05 19:45:16 +0530

    Insert module made

--- python_firebase_insert.py
+++ python_firebase_insert.py
@@ -1,2 +1,24 @@
+"""
+Prequisets:
+1. pip install requests
+2. pip install python-firebase
+>>Create an project in firebase
+"""
+
+
 from firebase import firebase
 
+
+"""This will make the connection to firebase DB file"""
+firebase =  firebase.FirebaseApplication("https://learn-python-firebase-dc967.firebaseio.com/", None)
+
+"""Data that you eant to send to firebase table"""
+data_document = {
+    'Name': 'John Wick',
+    'Location': 'New York',
+    'Killed' : 211
+}
+
+"""Using POst method you can send to cloud firebase"""
+result = firebase.post("/learn-python-firebase-dc967/MyFuckingTable", data_document)
+print(result)
